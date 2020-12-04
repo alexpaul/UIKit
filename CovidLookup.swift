a7596a5497ee0aa4902bdf1f94fd39997ce58a1b
Alex Paul <alexpaul@users.noreply.github.com>
2020-12-03 20:27:52 -0500

    Create CovidLookup.swift

---
+++ CovidLookup.swift
@@ -0,0 +1,20 @@
+# CovidLookup
+
+## Objective
+
+* Creating a Swift model(s) from Web API JSON data.
+* Using `URLSession` natively. 
+* Populating a Table View with parsed JSON data. 
+
+## COVID API
+
+1. Given the following endpoint: `https://api.covid19api.com/summary` verify the JSON payload via Postman or other. 
+1. Create a model(s) and an API client using the endpoint above. 
+1. Convert JSON property names to Swifty names using `CodingKeys` as needed.
+1. Populate a Table View with the `countries` array from the JSON data. 
+
+![covid lookup](https://user-images.githubusercontent.com/1819208/101109279-85c32700-35a4-11eb-9f58-864bbc5fdf5a.png)
+
+Bonus: 
+1. Add a search bar to the view controller. 
+2. The user should be able to search by country name e.g `Saint Lucia`
